7c289d213942601fa201f377e36b757dd949df39
Kasimis <petekasimis224@gmail.com>
2026-09-08 18:41:07 +0300

    Implement Retrieval function

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 
+
 # load the dataset
+
 dataset = []
 with open('data/cat-facts.txt', 'r', encoding='utf-8') as file:
     dataset = file.readlines()
     print(f'Loaded {len(dataset)} entries')
+
+
+
+#implement vector db
+
+import ollama
+
+EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+
+# Each element in the VECTOR_DB will be a tuple (chunk, embedding)
+# The embedding is a list of floats, for example: [0.1, 0.04, -0.34, 0.21, ...]
+VECTOR_DB = []
+
+def add_chunk_to_database(chunk):
+  embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
+  VECTOR_DB.append((chunk, embedding))
+
+for i, chunk in enumerate(dataset):
+  add_chunk_to_database(chunk)
+  print(f'Added chunk {i+1}/{len(dataset)} to the database')
+
+
+#implement the retrieval function
+
+#cosine similarity calculation
+def cosine_similarity(a, b):
+  dot_product = sum([x * y for x, y in zip(a, b)])
+  norm_a = sum([x ** 2 for x in a]) ** 0.5
+  norm_b = sum([x ** 2 for x in b]) ** 0.5
+  return dot_product / (norm_a * norm_b)
+
+#retrieval function
+def retrieve(query, top_n=3):
+  query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)['embeddings'][0]
+  # temporary list to store (chunk, similarity) pairs
+  similarities = []
+  for chunk, embedding in VECTOR_DB:
+    similarity = cosine_similarity(query_embedding, embedding)
+    similarities.append((chunk, similarity))
+  # sort by similarity in descending order, because higher similarity means more relevant chunks
+  similarities.sort(key=lambda x: x[1], reverse=True)
+  # finally, return the top N most relevant chunks
+  return similarities[:top_n]
+
